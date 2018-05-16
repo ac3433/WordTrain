58e3d199a9ddb7551a69b87eba04bbee014b7690
Hayden McPhee <hfm24@drexel.edu>
2018-05-16 17:56:12 -0400

    Added UI to new UITest scene.

---
+++ WordTrain/Assets/Script/UI/Slider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slider : MonoBehaviour {
+
+    public Vector3 newPos;
+    Vector3 oldPos;
+    bool toggled = false;
+    RectTransform rectTransform;
+
+	// Use this for initialization
+	void Start () {
+        rectTransform = gameObject.GetComponent<RectTransform>();
+        oldPos = rectTransform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Toggle()
+    {
+        if (toggled)
+        {
+            rectTransform.localPosition = oldPos;
+            toggled = false;
+        } else
+        {
+            rectTransform.localPosition += newPos;
+            toggled = true;
+        }
+    }
+}
---
+++ WordTrain/Assets/Script/UI/UIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+    public GameObject MainMenuPanel;
+
+	public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        MainMenuPanel.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        MainMenuPanel.SetActive(true);
+    }
+}
