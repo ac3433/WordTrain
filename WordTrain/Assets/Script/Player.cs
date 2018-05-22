5caa64dfaf5f2ca3e19d85263542ad0d793936ed
Alexander <ac3433@drexel.edu>
2018-05-22 03:25:31 -0400

    adding 2 player functions

---
+++ WordTrain/Assets/Script/Player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject hand;
+
+    public int totalPoint { get; private set; }
+    public int earnedPoint { get; private set; }
+    //enable the cards in the hand to be draggable.
+    public void UnlockHand()
+    {
+        if(hand != null)
+        {
+            foreach(Transform child in hand.transform)
+            {
+                CardControl cac = child.GetComponent<CardControl>();
+                if (cac != null)
+                    cac.isLock = false;
+            }
+        }
+    }
+
+    //disable the cards in the hands to be drag
+    public void LockHand()
+    {
+        if (hand != null)
+        {
+            foreach (Transform child in hand.transform)
+            {
+                CardControl cac = child.GetComponent<CardControl>();
+                if (cac != null)
+                    cac.isLock = true;
+            }
+        }
+    }
+
+    public void AddScore(int points)
+    {
+        earnedPoint = points;
+        totalPoint += points;
+    }
+
+}
---
+++ WordTrain/Assets/Script/TurnManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnManager : MonoBehaviour
+{
+    #region Singleton
+    protected static TurnManager _instance;
+    //Used only once to ensure when one thread have access to create the instance
+    protected static readonly object _Lock = new object();
+
+    public static TurnManager Instance
+    {
+        get
+        {
+            //thread safe!
+            lock (_Lock)
+            {
+                if (_instance != null)
+                    return _instance;
+                TurnManager[] instances = FindObjectsOfType<TurnManager>();
+                //see if there are any already more instance of this
+                if (instances.Length > 0)
+                {
+                    //yay only 1 instance so give it back
+                    if (instances.Length == 1)
+                        return _instance = instances[0];
+
+                    //remove all other instance of it other than the 1st one
+                    for (int i = 1; i < instances.Length; i++)
+                        Destroy(instances[i]);
+                    return _instance = instances[0];
+                }
+
+                GameObject manage = new GameObject("TurnManager");
+                manage.AddComponent<TurnManager>();
+
+                return _instance = manage.GetComponent<TurnManager>();
+            }
+        }
+    }
+    #endregion
+
+    [SerializeField]
+    private Player playerOne;
+    [SerializeField]
+    private Player playerTwo;
+    [SerializeField]
+    private GameObject canvas; //this is to rotate the canvas than the camera. Camera make it look funky.
+    
+    private Player currentPlayer;
+    
+    public void NextTurn()
+    {
+        RotateView();
+        SwitchPlayer();
+    }
+
+    private void SwitchPlayer()
+    {
+        if(currentPlayer.Equals(playerOne))
+        {
+            currentPlayer.LockHand();
+            currentPlayer = playerTwo;
+            currentPlayer.UnlockHand();
+        }
+        else
+        {
+            currentPlayer.LockHand();
+            currentPlayer = playerOne;
+            currentPlayer.UnlockHand();
+        }
+    }
+
+    private void RotateView()
+    {
+        if(canvas != null)
+            canvas.transform.Rotate(Vector3.forward, 180);
+    }
+
+        
+}
